65e1d573cc4565d9b080e98eaf59d4ec44828315
Admin <shivendrawk@gmail.com>
2025-02-22 21:21:36 +0530

    study of local scopes of nested functions

--- Script.js
+++ Script.js
@@ -1,18 +1,34 @@
-function a() {
-    let hi = "hello";
-    console.log(hi)
-    let so = 3;
-    var so2 = 4;
+// function a() {
+//     let hi = "hello";
+//     console.log(hi)
+//     let so = 3;
+//     var so2 = 4;
  
-    return hi;
-}
-a();
+//     return hi;
+// }
+// a();
 
-function b(c = "hi") {
-    console.log(c)
-}
-b()
+// function b(c = "hi") {
+//     console.log(c)
+// }
+// b()
 
 function nestedCall() {
-    
+    let outerVar = "out";
+    let outerVar2 = "out2";
+
+    function inside() {
+        let innerVar1 = "I'm inside";
+        console.log(outerVar);
+
+        function nestedInner() {
+            let innerVar2 = "I'm grand inside";
+            console.log(innerVar1);
+        }
+        nestedInner()
+    }
+    inside();
+
+    return outerVar;;
 }
+nestedCall();
